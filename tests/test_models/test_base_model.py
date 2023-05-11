c71d40e8eafa0f7e01ba683865825614dd1bbc6f
Dev-wonderful <adeonederful20@gmail.com>
2023-05-11 10:50:46 +0000

    BaseModel test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,7 +2,13 @@ import unittest
 from models.base_model import BaseModel
 
 
-class Test_BaseModel(unittest.Testcase):
+class Test_BaseModel(unittest.TestCase):
 
     def test_id(self):
         b1 = BaseModel
+        self.assertTrue(b1.id)
+        self.assertIsInstance(b1.id, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
