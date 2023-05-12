75ca3414a9f36acc9c78190006a2764144980ae4
Dev-wonderful <adeonederful20@gmail.com>
2023-05-12 01:04:15 +0000

    file storage methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """A module that handles persistance of data"""
+import json
 
 
 class FileStorage:
@@ -13,8 +14,24 @@ class FileStorage:
         pass
 
     def all(self):
-        """returns the dictionary of objects"""
+        """returns the storage dictionary of objects"""
         return __objects
 
     def new(self, obj):
-        """set"""
+        """adds a new object to the storage dictionary of objects
+        Args:
+            obj (obj): The object to be added
+        """
+        key = ".".join([(obj.__clas__.__name__), obj.id])
+        __object[key] = obj.to_dict()
+
+    def save(self):
+        """serializes storage dictionary to JSON file"""
+        with open(__file_path, 'w', encoding="utf-8") as json_file:
+            json.dump(__objects, json_file)
+
+    def reload(self):
+        """deserializes the JSON file to storage dictionary, file path exists"""
+        with open(__file_path, 'r', encoding="utf-8") as json_file:
+            json.load(json_file)
+
