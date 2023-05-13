7661411f5f21a42cb12f76d2280e1f981e66b795
Wonderful <adeonederful20@gmail.com>
2023-05-13 22:45:20 +0100

    user init

--- models/user.py
+++ models/user.py
@@ -3,12 +3,15 @@
 from models.base_model import BaseModel
 from models import storage
 
-"""" A class User that inherits from the BaseModel class"""
-class User(BaseModel):
-""" Public class attributes"""
 
-    email = " "
-    password = " "
-    first_name = " "
-    last_name = " "
-        
+class User(BaseModel):
+    """" A class User that inherits from the BaseModel class"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializing an instance and calling the super class"""
+        super().__init__(*args, **kwargs)
