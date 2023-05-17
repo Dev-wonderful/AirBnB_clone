fc95fc5371cfcab0de2ddd8fb166b6235bb835ea
Dev-wonderful <adeonederful20@gmail.com>
2023-05-17 16:00:33 +0100

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -7,7 +7,7 @@ from unittest import mock
 """" The working modules """
 
 
-class TestUser(unittest.TestCase):
+class TestReview(unittest.TestCase):
     """ Test for the User class"""
 
     @mock.patch('models.storage')
